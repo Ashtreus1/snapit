3f2697f5c6ea31ca0ef19f1a2a393b283b145b99
Ashtreus1 <yukanashi15@gmail.com>
2025-06-19 05:32:34 +0800

    tags completed

--- scripts/seed_tags.sql
+++ scripts/seed_tags.sql
@@ -1,6 +1,6 @@
 INSERT INTO tags (name)
 VALUES 
-('Nature'), ('City'), ('Portrait'), ('Food'), ('Travel'),
-('Pets'), ('Architecture'), ('Street'), ('Beach'), ('Mountains'),
-('Sunset'), ('Rain'), ('Nightlife'), ('Festival'), ('Cars'),
-('Abstract'), ('Black & White'), ('Wildlife'), ('Macro'), ('Fashion');
+('Abstract'), ('Architecture'), ('Beach'), ('BW'), ('Car'),
+('City'), ('Fashion'), ('Festival'), ('Food'), ('Macro'),
+('Mountain'), ('Nature'), ('Night'), ('Pets'), ('Portrait'),
+('Rain'), ('Street'), ('Sun'), ('Travel'), ('Wild');
